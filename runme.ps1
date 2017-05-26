1f98b7c8a039fd6a9160a3c9d5194b09d7955caa
itsokov <ivaylo.tsokov@hpe.com>
2017-05-26 14:13:41 +0000

    test

--- runme.ps1
+++ runme.ps1
@@ -33,7 +33,7 @@ if (Get-Module -Name $moduleName)
 }
 #endregion reload module
 
-Export-SPRObjects -Path $exportPath
+#Export-SPRObjects -Path $exportPath
 Get-SPReport -ExportPath $exportPath -FromExportedFiles -ReportFilePath $reportFilePath
 Invoke-Item $reportFilePath
 
